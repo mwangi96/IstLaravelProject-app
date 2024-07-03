84746c9fc0f9b4e05abc0f6b8a115b771072a359
Mwangi <gideonmwangi718@gmail.com>
2024-07-04 02:28:21 +0300

    PermissionController successfully edited

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+
+    // public function __construct()
+    // {
+    //     $this-> middleware('permission:view permission', ['only' => ['index']]);
+    //     $this-> middleware('permission:create permission', ['only' => ['create','store']]);
+    //     $this->middleware('permission:update permission', ['only' => ['update','edit']]);
+    //     $this->middleware('permission:delete permission', ['only' => ['destroy']]);
+    // }
+    public function index()
+    {
+        $permissions = Permission::get();
+        return view('role-permission.permission.index', [
+            'permissions' => $permissions
+        ]);
+    }
+
+    public function create()
+    {
+        return view('role-permission.permission.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' =>
+            'required',
+            'string',
+            'unique:permissions,name'
+        ]);
+
+        Permission::create([
+            'name' => $request->name
+        ]);
+
+        return redirect('permissions')->with('status', 'Permission created successfully');
+    }
+
+    public function edit(permission $permission)
+    {
+        return view('role-permission.permission.edit', [
+            'permission' => $permission
+        ]);
+    }
+
+    public function update(Request $request, Permission $permission)
+    {
+        $request->validate([
+            'name' =>
+            'required',
+            'string',
+            'unique:permissions,name,'.$permission->id
+        ]);
+
+        $permission->update([
+            'name' => $request->name
+        ]);
+
+        return redirect('permissions')->with('status', 'Permission updated successfully');
+    }
+
+    public function destroy($permissionId)
+    {
+        $Permission = Permission::find($permissionId);
+        $Permission->delete();
+        return redirect('permissions')->with('status', 'Permission deleted successfully');
+
+    }
+} 
