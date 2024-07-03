ca7266e291efd5cf5b26715fa8e62bd34b4abce7
Mwangi <gideonmwangi718@gmail.com>
2024-07-04 02:39:22 +0300

    main application for the laravel spatie roles and permissions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\JobController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+// use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+
+
+
+// Route::group(['middleware' => ['role:super-admin|admin']], function() {
+
+    Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+    Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class, 'destroy']);
+
+    Route::resource('roles', App\Http\Controllers\RoleController::class);
+    Route::get('roles/{roleId}/delete', [App\Http\Controllers\RoleController::class, 'destroy']);
+    Route::get('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'addPermissionToRole']);
+    Route::put('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'updatePermissionToRole']);
+
+    Route::resource('users', App\Http\Controllers\UserController::class);
+    Route::get('users/{userId}/delete', [App\Http\Controllers\UserController::class, 'destroy']);
+
+
+    // Route::resource('jobs', JobController::class);
+
+
+// });
 
 Route::get('/', function () {
     return view('welcome');
